eae690c5548df06c7fc22d4bd79312c9028fb53c
Andre Hartmann <andre@zalando.de>
2018-06-07 14:27:39 +0200

    remove unused import

--- zmon-actuator/src/test/java/org/zalando/zmon/actuator/backend/ZmonRestBackendMetricsTest.java
+++ zmon-actuator/src/test/java/org/zalando/zmon/actuator/backend/ZmonRestBackendMetricsTest.java
@@ -14,7 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 import org.zalando.zmon.actuator.ExampleApplication;
